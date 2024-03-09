9f028c1b28d1125ae3131674ef26b616ece1e71c
Nicholas Liem <nicholas.liem@shippit.com>
2024-03-09 19:04:46 +0700

    feat: add splash screen and app icon

---
+++ app/src/main/java/com/ktl/bondoman/ui/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.ktl.bondoman.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.ktl.bondoman.MainActivity
+import com.ktl.bondoman.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
