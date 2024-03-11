30787083264fd6a5215068fc4cac5e2a6ab6a350
Gulilil <juan.csantoso@gmail.com>
2024-03-11 13:58:01 +0700

    feat: loading screen animation and logo change

--- app/src/main/java/com/ktl/bondoman/ui/SplashActivity.kt
+++ app/src/main/java/com/ktl/bondoman/ui/SplashActivity.kt
@@ -1,9 +1,11 @@
 package com.ktl.bondoman.ui
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import com.ktl.bondoman.MainActivity
 import com.ktl.bondoman.R
@@ -11,10 +13,18 @@ import com.ktl.bondoman.R
 @SuppressLint("CustomSplashScreen")
 @Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
-
+    lateinit var progressBar : ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        progressBar = findViewById(R.id.progressBar)
+
+        progressBar.max = 1000;
+        val curProgress = 1000;
+
+        ObjectAnimator.ofInt(progressBar, "progress", curProgress)
+            .setDuration(1000)
+            .start()
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
